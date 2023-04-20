0e05996049915a55e1e73efc99f6fdaad9cf41b6
Giovanni Medeiros <ggabriel.medeiros@hotmail.com>
2023-04-20 18:12:59 -0300

    inclusão footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,25 +6,36 @@ function Footer() {
       <div className="footer-sobre-nos">
         <h3>SOBRE NOS</h3>
         <ul>
-            <li>CONHEÇA</li>
-            <li>COMO COMPRAR</li>
-            <li>INDICAÇÃO E DESCONTO</li>
+          <li>CONHEÇA</li>
+          <li>COMO COMPRAR</li>
+          <li>INDICAÇÃO E DESCONTO</li>
         </ul>
       </div>
 
       <div className="footer-informacoes">
         <h3>INFORMAÇÕES UTEIS</h3>
         <ul>
-            <li>FALE CONOSCO</li>
-            <li>DÚVIDAS</li>
-            <li>PRAZOS DE ENTREGA</li>
-            <li>FORMAS DE PAGAMENTO</li>
-            <li>POLITICA DE PRIVACIDADE</li>
-            <li>TROCAS E DEVOLUÇÕES</li>
+          <li>FALE CONOSCO</li>
+          <li>DÚVIDAS</li>
+          <li>PRAZOS DE ENTREGA</li>
+          <li>FORMAS DE PAGAMENTO</li>
+          <li>POLITICA DE PRIVACIDADE</li>
+          <li>TROCAS E DEVOLUÇÕES</li>
         </ul>
       </div>
       <div className="footer-news">
-        <img src={News} alt="new" className='footer-news-img'/>
+        <div className='footer-news-title'>
+          <h3>CADASTRE-SE E RECEBA NOSSAS <strong>NOVIDADES E PROMOÇÕES</strong></h3>
+        </div>
+
+        <div className='footer-news-description'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, provident.tempore laboriosam quis voluptate eius ad ipsa asperiores aperiam ipsam?</p>
+        </div>
+
+        <div className='footer-news-input'>
+          <input className='button-input' type="text" placeholder='Digite seu e-mail'/>
+          <input className='button-input-ok' type="button" value="Ok" />
+        </div>
       </div>
     </footer>
   );
